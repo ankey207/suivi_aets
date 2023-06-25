f3843896d23f4d51fefa87816569e52fafa7cf43
ankey207 <amanyankey040@gmail.com>
2023-06-25 19:01:24 +0000

    seconf commit

--- myapp.py
+++ myapp.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
 from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
